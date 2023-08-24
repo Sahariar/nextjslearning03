7413bad2246b308d0de51a67128255bf8020600d
Sahariar Kabir <sronok@gmail.com>
2023-08-24 21:15:57 +0600

    user ssg work

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,39 +1,49 @@
 import getUser from "@/lib/getUser"
 import getUserPost from "@/lib/getUserPost"
-import {Suspense} from "react"
+import { Suspense } from "react"
 import UserPosts from "./components/UserPosts"
 import { Metadata } from "next"
+import getAllUsers from "@/lib/getAllUsers"
 type Params = {
-  params:{
-    userId:string
+  params: {
+    userId: string
   }
 }
-export async function generateMetadata({params:{userId}}:Params):Promise<Metadata> {
-  const userData:Promise<User> = getUser(userId)
-  const user:User = await userData
+export async function generateMetadata({ params: { userId } }: Params): Promise<Metadata> {
+  const userData: Promise<User> = getUser(userId)
+  const user: User = await userData
 
   return {
-    title:user.name,
-    description:`This is ${user.name}`
+    title: user.name,
+    description: `This is ${user.name}`
   }
 }
-export default async function User({params:{userId}}:Params) {
-  const userData:Promise<User> = getUser(userId)
-  const userPostData:Promise<Post[]> = getUserPost(userId);
+export default async function User({ params: { userId } }: Params) {
+  const userData: Promise<User> = getUser(userId)
+  const userPostData: Promise<Post[]> = getUserPost(userId);
   // const[user, userPosts] = await Promise.all([
   //   userData,userPostData
   // ])
   const user = await userData
   return (
     <>
-    <div className="container mx-auto">
-      <h2 className="text-2xl">
-        {user.name}
-      </h2>
-      <Suspense fallback={<h2 className="text-xl"> Loading....</h2>}>
-        <UserPosts promise = {userPostData} />  
-      </Suspense>
-    </div>
+      <div className="container mx-auto">
+        <h2 className="text-2xl">
+          {user.name}
+        </h2>
+        <Suspense fallback={<h2 className="text-xl"> Loading....</h2>}>
+          <UserPosts promise={userPostData} />
+        </Suspense>
+      </div>
     </>
+  )
+}
+export async function generateStaticParams() {
+  const usersData: Promise<User[]> = getAllUsers()
+  const users = await usersData
+
+  return users.map(user => ({
+    userId: user.id.toString()
+  })
   )
 }
--- lib/getUser.tsx
+++ lib/getUser.tsx
@@ -1,5 +1,6 @@
 export default async function getUser(userId:String) {
-        const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+        const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`,
+        {next:{revalidate:60}});
         if(!res.ok) throw new Error("Failed to fetch User");
       return res.json();
     }
